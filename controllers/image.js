68ba1b981e31c5ea7442deaebfbfb9829469460f
8rianHo <brian8ho@googlemail.com>
2018-05-03 21:37:39 +0100

    apikey to heroku

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'aca6aded4f6548598db11295993b9839'
+    apiKey: process.env.CLARIFAI_KEY
 });
 
 const handleImageURL = (req, res) => {
